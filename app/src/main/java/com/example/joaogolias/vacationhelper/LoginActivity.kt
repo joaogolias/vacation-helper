6b5e8aa7034be0545f13c439b38c72559601633c
João Golias <joaovgolias@gmail.com>
2018-12-22 21:02:58 -0200

    set login button function

--- app/src/main/java/com/example/joaogolias/vacationhelper/LoginActivity.kt
+++ app/src/main/java/com/example/joaogolias/vacationhelper/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.example.joaogolias.vacationhelper
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login_button.setOnClickListener {
+            this.login();
+        }
+    }
+
+    fun login(){
+        var intent = Intent(this, HomeActivity::class.java);
+        startActivity(intent);
     }
 }
