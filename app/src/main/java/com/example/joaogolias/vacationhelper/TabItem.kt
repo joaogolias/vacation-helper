3492ca442b3e42f83d8fcbd0a77a04160db70afd
João Golias <joaovgolias@gmail.com>
2018-12-23 21:12:24 -0200

    removed unused println

--- app/src/main/java/com/example/joaogolias/vacationhelper/TabItem.kt
+++ app/src/main/java/com/example/joaogolias/vacationhelper/TabItem.kt
@@ -14,28 +14,26 @@ import android.support.v4.content.ContextCompat.getSystemService
 
 
 class TabItem(context: Context, private val attrs: AttributeSet) : LinearLayout(context, attrs) {
-    private var icon: Drawable? = null
 
     init  {
+
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         inflater.inflate(R.layout.tab_item, this, true)
 
         val styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.TabItem)
         styledAttributes?.let {
             for(index in 0..(it.length() - 1)) {
-                println("it.length: ${it.length()}")
                 val iconInteger = styledAttributes.getIndex(index)
-                println("iconInteger: $iconInteger")
                 if(iconInteger.equals(R.styleable.TabItem_icon)) {
                     val iconReference = styledAttributes.getDrawable(R.styleable.TabItem_icon)
-                    this.icon = iconReference
+
                     icon_image_view?.let {
                         icon_image_view.setImageDrawable(iconReference)
                     }
                 }
             }
-            it.recycle()
         }
+        styledAttributes.recycle()
 
     }
 }
